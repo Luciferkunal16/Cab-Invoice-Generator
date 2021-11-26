52a76529aea223bd6faa29fdfabf3bd55ec0d489
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-26 16:09:40 +0530

    Added UC 4 : Invoice Service with help of Arraylist and map

--- src/main/CabInvoiceGenerator.java
+++ src/main/CabInvoiceGenerator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CabInvoiceGenerator {
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
@@ -16,12 +18,12 @@ public class CabInvoiceGenerator {
         }
         return  totalFare;
     }
-    public InvoiceSummary calculateFareReturnObject(Ride[] rides) {
+    public InvoiceSummary calculateFareReturnObject(ArrayList<Ride> rides) {
 
         double totalFare = 0;
         for(Ride ride : rides) {
             totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
         }
-        return new InvoiceSummary(rides.length, totalFare);
+        return new InvoiceSummary(rides.size(), totalFare);
     }
 }
